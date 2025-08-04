ed1b22e13dd17dcc302608fefc05a875cbcebf26
Muhammad Abdullah Amjad <abdullahamjadlhr2020@gmail.com>
2025-08-04 19:59:13 +0500

    Update 4/8/25

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
   static const String appName = 'Todo App';
-  static const String baseUrl = 'https://todo-nodejs-flutter-app.vercel.app';
+  static const String baseUrl = '';
 
   // API Endpoints
   static const String loginEndpoint = '/login';
